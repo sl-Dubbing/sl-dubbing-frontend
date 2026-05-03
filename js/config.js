6cb6d6d321714e580e507c3806be60f5c42c8621
sl-Dubbing <alhlil.ma@gmail.com>
2026-05-03 04:07:03 +0300

    Add APP_CONFIG object with Supabase key

--- js/config.js
+++ js/config.js
@@ -1,3 +1,6 @@
 // config.js - Supabase anon public key (safe for browser)
 // WARNING: Do NOT upload this file to GitHub!
-window.__SUPABASE_KEY__ = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNramtreHJsZ2lzamRvbHdkZGZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0NjU0OTUsImV4cCI6MjA5MzA0MTQ5NX0.F-4TbmO6_7plPm8NBr_6djCv6gtEPpWFw9J7m8vTs6M";
+
+window.APP_CONFIG = {
+    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNramtreHJsZ2lzamRvbHdkZGZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0NjU0OTUsImV4cCI6MjA5MzA0MTQ5NX0.F-4TbmO6_7plPm8NBr_6djCv6gtEPpWFw9J7m8vTs6M"
+};
