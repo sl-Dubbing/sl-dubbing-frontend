1111bfcb4bf7e7849608554f624ccf08d97aee66
sl-Dubbing <alhlil.ma@gmail.com>
2026-05-10 21:23:59 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // js/config.js
 window.APP_CONFIG = {
-    API_BASE: "https://web-production-14a1.up.railway.app",
+    API_BASE: "https://api.glotix.ai",
     SUPABASE_URL: "https://ckjkkxrlgisjdolwddfg.supabase.co",
     SUPABASE_KEY: "sb_publishable_vS3koY6oKGMH16u1DdtLrg_PC83FaHW"
 };
